bb8373fc96dace7d0abeaf69ae4a4c536d314c85
k-indhirani <indhirani113@gmail.com>
2026-05-05 11:26:58 +0530

    Update dashboard.py.py

--- dashboard.py.py
+++ dashboard.py.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.title("INTERNSHIP PROJECT ANALYTICS DASHBOARD - K.INDRANI")
 
 df = pd.read_csv('internship_data.csv')
-df['Start_Date'] = pd.to_Datetime(df['Start_Date'])
+df['Start_Date'] = pd.to_datetime(df['Start_Date'])
 
 dept = st.sidebar.multiselect(
     "Department Filter Pannu:", 
